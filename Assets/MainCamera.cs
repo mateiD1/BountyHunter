0447b24bd41a156900a3b6931a534aab5b743042
mateiD1 <damian.matei09@icloud.com>
2026-09-23 20:42:09 +0200

    PROTO Basic turning and moving

--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    Camera _camera;
+    private static Camera _camera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _camera = GetComponent<Camera>();
+        _camera.transform.position.Set(0f, 6f, -7f);
     }
 
     void FixedUpdate()
     {
         
+    }
+
+    void Update()
+    {
+        checkMouseInput();
+    }
+
+    void checkMouseInput()
+    {   
+        float v = Input.GetAxis("Mouse Y") * 0.5f;
+
+        // negative so when moving mouse up, we look up
+        transform.Rotate(-v, 0, 0);
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,15 +1,25 @@
 using System;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     private static Rigidbody _playerBody;
+    private bool _canJump;
     void Start()
     {
         _playerBody = GetComponent<Rigidbody>();
+        _playerBody.MovePosition(new Vector3(0f, 2f, 0f));
+        _playerBody.mass = 10f;
+        _playerBody.detectCollisions = true;
+        
+        _canJump = true;
     }
 
     void FixedUpdate()
@@ -17,5 +27,34 @@ public class Player : MonoBehaviour
         
     }
 
-    public static Vector3 GetPlayerPosition () => _playerBody.transform.position;
+    void Update()
+    {
+        checkKeyboardInputs();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        _canJump = true;
+    }
+
+    void checkKeyboardInputs()
+    {
+        float jump = 0f;
+        if (Input.GetKey(KeyCode.Space) && _canJump)
+        {
+            jump = 30f;
+            _canJump = false;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float mouseY = Input.GetAxis("Mouse X") * 0.5f;
+
+        Vector3 movement = new Vector3(horizontal, jump, vertical);
+        float speed = 20f;
+        transform.Translate(movement * speed * Time.deltaTime);
+        transform.Rotate(0, mouseY, 0);
+
+        // mouse should remain in the middle, so it never hits the wall and the user can turn endlessly
+    }
 }
